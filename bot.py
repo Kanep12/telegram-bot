ae99f2f9bdcf659012d9dab74831fea90c69bde0
Kanep12 <114320820+Kanep12@users.noreply.github.com>
2025-12-10 00:51:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,15 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TOKEN")  # Token loetakse Railway environment variable'st
-LINK = "Test"  # siia pane link, mida bot saadab
-
+TOKEN = os.getenv("TOKEN")
+LINK = "https://example.com"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Siin on su link: {LINK}")
 
-
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Bot töötab!")
-    await app.run_polling()
-
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    print("Bot töötab!")
+    app.run_polling()
